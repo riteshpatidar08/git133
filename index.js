32c952159211e0f88314194cf2c41d84b890bacf
ritesh <riteshpatidar088@gmail.com>
2026-09-18 15:47:30 +0530

    script js added

--- index.js
+++ index.js
@@ -2,3 +2,23 @@ const firstName = 'ritesh';
 const age = 30 ;
 const email = 'ritesh@gmail.com'
 // git add => jitni bhi files availabel ya koi particular file usko staging area main move krdegi 
+
+
+//NOTE  git init => initialize a local repo.
+//NOTE  git status => check the untracked and modified file in the current folder. 
+//NOTE  git add . => added the changes into staging area.
+//NOTE  git commit -m 'msg'=> commit means version banana / it will create a new version of our code.
+//NOTE  what is vcs ?
+//NOTE  what is git ?
+//NOTE  what is the difference between git and github 
+//NOTE  what is repositatory ? 
+//NOTE  what is staging area ?
+//NOTE  what is commit ?
+//NOTE  what does status cmd do?
+//NOTE  what does git init do?
+//NOTE  what is the use of commit msg ?
+//NOTE  what is the use of git log ?
+//NOTE git repo called (localrepo) exist only in our system 
+//NOTE remote repo / github
+//NOTE git checkout commitHash  (use to check speficic commit code) 
+//NOTE git checkout master => use to switch back to current code )
